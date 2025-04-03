cfbf7a0779e01526817b295a130b9c1cbda0fdac
wly-wang <s2106059@phcomputeppe01.ph.ed.ac.uk>
2025-04-03 17:15:55 +0100

    .

--- larana/OpticalDetector/OptDetDigitizer_module.cc
+++ larana/OpticalDetector/OptDetDigitizer_module.cc
@@ -216,7 +216,7 @@ namespace opdet {
     optdata::ChannelDataGroup rawWFGroup_LowGain(optdata::kLowGain);
     // Reserve entries equal to # of channels
     // auto const nOpChannels = fWireReadoutGeom->NOpChannels();
-    auto const nOpChannels = art::ServiceHandle<geo::Geometry>()->NOpDets();
+    auto const nOpChannels = art::ServiceHandle<geo::GeometryCore>()->NOpDets();
     rawWFGroup_HighGain.reserve(nOpChannels);
     rawWFGroup_LowGain.reserve(nOpChannels);
 
